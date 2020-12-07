588188b17c0b33ccf01d2996446a6813e0d46148
Gemma Sharp <gs8094@bristol.ac.uk>
2020-12-07 20:27:08 +0000

    Update alspac.sh
    amend mistakes

--- making_key/alspac.sh
+++ making_key/alspac.sh
@@ -5,11 +5,11 @@
 
 module add languages/R-4.0.3-gcc9.1.0
 
-mkdir - p ~/EPoCH/data/
+mkdir -p ~/EPoCH/data/
 
-mkdir - p ~/EPoCH/scripts/
+mkdir -p ~/EPoCH/scripts/
 
-mkdir - p ~/EPoCH/out/
+mkdir -p ~/EPoCH/out/
 
 cp /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/alspac_pheno.rds ~/EPoCH/data/
 
