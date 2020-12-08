a1ee34894f7854a3dcc1fb71a979f89f9512029b
Gemma Sharp <gs8094@bristol.ac.uk>
2020-12-08 19:22:54 +0000

    Update bash_master.sh

--- bash_master.sh
+++ bash_master.sh
@@ -3,11 +3,17 @@
 #
 #PBS -l nodes=1:ppn=1,walltime=12:00:00
 
-cohort=echo "$cohort"
 script=echo "$script"
+cohort=echo "$cohort"
+
+echo $script
+echo $cohort
 
 scriptlocation="~/EPoCH/scripts/${script}.R"
 outfile="~/EPoCH/out/${cohort}_${script}.out"
+
+echo $scriptlocation
+echo $outfile
 
 module add languages/R-4.0.3-gcc9.1.0
 
