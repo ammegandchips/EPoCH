affa8a01e8ecd6cc0b059a898c9afdfcbde72ef8
Gemma Sharp <gs8094@bristol.ac.uk>
2020-12-09 18:03:10 +0000

    Update after.sh

--- after.sh
+++ after.sh
@@ -2,19 +2,19 @@ cp ~/EPoCH/results/alspac_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/01
 cp ~/EPoCH/results/bib_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/
 cp ~/EPoCH/results/mcs_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/
 
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/meta_analysis/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/meta_analysis/results/
 
 cp ~/EPoCH/results/alspac_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 cp ~/EPoCH/results/bib_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
 cp ~/EPoCH/results/mcs_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 cp ~/EPoCH/results/meta_analysis* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/meta_analysis/
 
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/data_checks/
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/data_checks/
-mkdir -p ~/projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/data_checks/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/data_checks/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/data_checks/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/data_checks/
 
 cp ~/EPoCH/results/alspac_checks* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/data_checks/
 cp ~/EPoCH/results/bib_checks* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/data_checks/
