603feb43fe197bf0f4b4c329fd2e9547ef8f8cd8
Gemma Sharp <gs8094@bristol.ac.uk>
2021-01-29 18:35:25 +0000

    Update after.sh

--- after.sh
+++ after.sh
@@ -9,9 +9,11 @@ mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/result
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/meta_analysis/results/
 
-cp ~/EPoCH/results/alspac_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
-cp ~/EPoCH/results/bib_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
-cp ~/EPoCH/results/mcs_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
+cp ~/EPoCH/results/ALSPAC_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
+cp ~/EPoCH/results/BIB_ALL_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+cp ~/EPoCH/results/BIB_SA_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+cp ~/EPoCH/results/BIB_WE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+cp ~/EPoCH/results/MCS_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 cp ~/EPoCH/results/meta_analysis* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/meta_analysis/
 
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/data_checks/
