a506b6c33df7e3de8c3f7edbf0eb22bf1d6ef7f2
Gemma Sharp <gs8094@bristol.ac.uk>
2022-01-07 18:40:38 +0000

    Update epoch_master.sh

--- epoch_master.sh
+++ epoch_master.sh
@@ -3,27 +3,29 @@
 #SBATCH --partition=gpu_short
 #SBATCH --gres=gpu:2
 #SBATCH --job-name=epoch_master
-#SBATCH --output=epoch_master_out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH -mem=2000M
-#SBATCH --time=06:00:00
+#SBATCH --time=6:0:0
+#SBATCH --mem=2000M
 
 echo 'epoch_master'
 hostname
 
-scriptname=echo "$scriptname"
-cohort=echo "$cohortORmodel"
+cohortORmodel=${1}
+scriptname=${2}
 
-scriptlocation="~/EPoCH/scripts/${scriptname}.R"
-outfile="~/EPoCH/out/${cohortORmodel}_${scriptname}.out"
+echo $cohortORmodel
+echo $scriptname
+
+scriptlocation="~/EPoCH/scripts/$scriptname.R"
+outfile="~/EPoCH/out/$cohortORmodel_$scriptname.out"
 
 echo $scriptlocation
 echo $outfile
 
 module add lang/r/4.0.3-bioconductor-gcc
 
-rscriptcommand="Rscript --verbose ${scriptlocation} ${cohortORmodel} >> ${outfile} 2>&1"
+rscriptcommand="Rscript --verbose $scriptlocation $cohortORmodel >> $outfile 2>&1"
 
 echo $rscriptcommand
 
