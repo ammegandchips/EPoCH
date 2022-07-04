64b584496a36e08bc3af86f996c0364666773294
Gemma Sharp <gs8094@bristol.ac.uk>
2022-07-04 18:33:03 +0100

    updating coefplots
    adding coefplot code and error messages

--- results_app2/tab4_server_coefplots.R
+++ results_app2/tab4_server_coefplots.R
@@ -12,7 +12,10 @@ tree_names <- paste0("exposure_tree",1:4)
 all_trees_and_headings <- vector("list",4)
 
 # When number of comparisons selected and GO button pressed, generate the required number of shiny trees
-observeEvent(input$go4,{
+observeEvent(input$tab4GoA,{
+  if(input$load_results==0){
+    showModal(modalDialog("You must load the data first"))
+  }else{
   all_trees_and_headings <- all_trees_and_headings[1:as.numeric(input$n_comparisons)]
   
   # Exposure variable 1
@@ -60,7 +63,7 @@ observeEvent(input$go4,{
 output$exposure_trees <- renderUI({
       tagList(all_trees_and_headings)
       })
-
+}
 })
 
 ##################################
@@ -72,49 +75,55 @@ exposure_selected_slice1<- reactive({
 })
 
 output$exposure_message1 <- renderText({
-  ifelse(ncol(exposure_selected_slice1()[[1]])<8,
+  ifelse(ncol(exposure_selected_slice1()[[1]])<7,
          "Select more sub-classes for variable 1 (follow the branch all the way to the end)",
          "")
 })
 
 exposure_selected_slice2<- reactive({
-  if (2 %in% 1:input$n_comparisons){
+  if (input$load_results==1){
+    if(2 %in% 1:input$n_comparisons){
     lapply(get_selected(input$exposure_tree2,format="slices"),function(node){treeToDf(node,exposure_hierarchy)})
   } else{
     "None"
   }
+  }
 })
 
 output$exposure_message2 <- renderText({
-  ifelse(ncol(exposure_selected_slice2()[[1]])<8,
+  ifelse(ncol(exposure_selected_slice2()[[1]])<7,
          "Select more sub-classes for variable 2 (follow the branch all the way to the end)",
          "")
 })
 
 exposure_selected_slice3<- reactive({
-  if (3 %in% 1:input$n_comparisons){
-    lapply(get_selected(input$exposure_tree3,format="slices"),function(node){treeToDf(node,exposure_hierarchy)})
-  } else{
-    "None"
+  if (input$load_results==1){
+    if(3 %in% 1:input$n_comparisons){
+      lapply(get_selected(input$exposure_tree3,format="slices"),function(node){treeToDf(node,exposure_hierarchy)})
+    } else{
+      "None"
+    }
   }
 })
 
 output$exposure_message3 <- renderText({
-  ifelse(ncol(exposure_selected_slice3()[[1]])<8,
+  ifelse(ncol(exposure_selected_slice3()[[1]])<7,
          "Select more sub-classes for variable 3 (follow the branch all the way to the end)",
          "")
 })
 
 exposure_selected_slice4<- reactive({
-  if (4 %in% 1:input$n_comparisons){
-    lapply(get_selected(input$exposure_tree4,format="slices"),function(node){treeToDf(node,exposure_hierarchy)})
-  } else{
-    "None"
+  if (input$load_results==1){
+    if(4 %in% 1:input$n_comparisons){
+      lapply(get_selected(input$exposure_tree4,format="slices"),function(node){treeToDf(node,exposure_hierarchy)})
+    } else{
+      "None"
+    }
   }
 })
 
 output$exposure_message4 <- renderText({
-  ifelse(ncol(exposure_selected_slice4()[[1]])<8,
+  ifelse(ncol(exposure_selected_slice4()[[1]])<7,
          "Select more sub-classes for variable 4 (follow the branch all the way to the end)",
          "")
 })
@@ -149,7 +158,77 @@ output$all_selected_exposures_df <-renderTable({
 ##################
 # COEF PLOT CODE #
 ##################
-# make a df containing all exposures/comparisons X
-# When generate plot button is pressed
-# subset all_res to just those exposures and the selected outcomes
-# draw coef plots
+
+## when the user clicks "Generate plot"...
+observeEvent(input$tab4GoB,{
+  ## if the data hasn't been loaded, they get an error message...
+  if(input$load_results==0){
+    showModal(modalDialog("You must load the data first"))
+    reset("tab4GoB")
+  }
+    # if the data have been loaded...
+    if(input$load_results==1){
+      print("You've loaded the data!")
+      #check whether the trees have been generated
+      if(input$tab4GoA==0){
+        showModal(modalDialog("You must generate the trees first"))
+        reset("tab4GoB")
+      }
+      if(input$tab4GoA==1){
+        print("you've generated the trees!!!")
+        print(input$n_comparisons)
+        # we make a list of the commands needed to find out whether all the trees have had nodes selected right to the end of the branches
+        tmp_list <- unlist(lapply(1:input$n_comparisons,function(i){paste0("try(ncol(exposure_selected_slice",i,"()[[1]])<7)")}))
+        print(tmp_list)
+        #then we evaluate those commands (giving a logical vector telling us whether any of them are incomplete)
+        tmp_list <- unlist(lapply(tmp_list,function(x){eval(parse(text=x))}))
+        print(tmp_list)
+        #if at least one of the trees is incomplete, an error message is generated
+          if(TRUE %in% tmp_list){
+          showModal(modalDialog("You need to select more nodes of the tree(s)"))
+          reset("tab4GoB")
+  ## but if that's all ok, they get a message saying that the visualisations are being generated...
+  }else{
+    showModal(modalDialog("Visualisations are shown on the next tab"))
+    ## then the visualisations appear...
+    output$CoefficientPlot <- renderPlotly({
+      df <- all_selected_exposures()
+      print(dim(df))
+      print(df)
+      models <- as.character(df["model",])
+      print(models)
+      df<-as.data.frame(t(df))
+      print(df)
+      exposure_linkers <- paste(df$exposure_class,df$exposure_subclass,df$exposure_time,df$person_exposed,df$exposure_type,df$exposure_source,sep="-")
+      if("exposure_dose" %in% colnames(df)){
+      print(df$exposure_dose)
+      df$exposure_dose[is.na(df$exposure_dose)]<-"NA"
+      }
+      if("exposure_dose" %in% colnames(df)==F){
+        df$exposure_dose<-"NA"
+      }
+      exposure_linkers <- paste(exposure_linkers,df$exposure_dose)
+      exposure_linkers <- exposure_linkers[is.na(models)==F]
+      print(exposure_linkers)
+      dat <- lapply(1:length(exposure_linkers),function(i){
+        loaded_data()$all_res[which(loaded_data()$all_res$model==models[i]&
+                                      loaded_data()$all_res$exposure_linker==exposure_linkers[i]),]
+      })
+      names(dat) <- paste("Comparison",1:length(exposure_linkers))
+      dat<-bind_rows(dat,.id="comparison")
+      print(dim(dat))
+      dat <- dat[dat$outcome_class==input$out_class_expcoef,]
+      print(dim(dat))
+      print(head(dat))
+      print(table(dat$comparison))
+      dat$lcl <- dat$est-(1.96 * dat$se)
+      dat$ucl <- dat$est+(1.96 * dat$se)
+      coef_plot <- ggplot(dat,aes(x=est,y=outcome_linker,xmin=lcl,xmax=ucl))+
+        geom_pointrange()+
+        facet_grid(.~comparison)
+      ggplotly(coef_plot)
+      })
+  }
+      }
+    }
+})
--- results_app2/tab4_ui_coefplots.R
+++ results_app2/tab4_ui_coefplots.R
@@ -5,16 +5,16 @@ tabPanel(title="Exposure Coef Plots",value="tab4",
                       column(3,
                         h4("How many comparisons do you want to make?"),
                         radioButtons("n_comparisons",label=NULL,choices = "Load results first"),
-                        actionButton("go4",label="Generate selection trees"),
+                        actionButton("tab4GoA",label="Generate selection trees"),
                         uiOutput("exposure_trees")
                      ),
                       column(3,offset = 1,
-                             h4("Select output"),
+                             h4("Select outcome class"),
                              radioButtons("out_class_expcoef",label=NULL,choices = "Load results first")
                       ),
                      column(3,
                             h4("Press when ready"),
-                            actionButton("go5",label="Generate plot")
+                            actionButton("tab4GoB",label="Generate plot")
                             )
                     )
            ),
@@ -29,7 +29,8 @@ tabPanel(title="Exposure Coef Plots",value="tab4",
                         span(textOutput("exposure_message2"), style="color:red"),
                         span(textOutput("exposure_message3"), style="color:red"),
                         span(textOutput("exposure_message4"), style="color:red"),
-                        tableOutput("all_selected_exposures_df")
+                        tableOutput("all_selected_exposures_df"),
+                        plotlyOutput("CoefficientPlot")
                       )
                     )
            )
