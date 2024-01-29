8c248ae5391d374af6f3609e319eb8aeafe7f60d
Gemma Sharp <gs8094@bristol.ac.uk>
2024-01-29 15:31:22 +0000

    Update after.sh
    adding bib sa and bib we

--- after.sh
+++ after.sh
@@ -1,22 +1,28 @@
 cp ~/EPoCH/results/alspac_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/
 cp ~/EPoCH/results/bib_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/
+cp ~/EPoCH/results/bibsa_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibsa/
+cp ~/EPoCH/results/bibwe_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibwe/
 cp ~/EPoCH/results/mcs_key.rds /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/
 
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibsa/results/
+mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibwe/results/
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 mkdir -p /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/meta_analysis/results/
 
 cp ~/EPoCH/results/ALSPAC_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 cp ~/EPoCH/results/BIB_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
+cp ~/EPoCH/results/BIBSA_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibsa/results/
+cp ~/EPoCH/results/BIBWE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bibwe/results/
 cp ~/EPoCH/results/MCS_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 cp ~/EPoCH/results/MOBA_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/moba/results/
 
 cp ~/EPoCH/results/ALSPAC_FEMALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 cp ~/EPoCH/results/BIB_FEMALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
 cp ~/EPoCH/results/MCS_FEMALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
-cp ~/EPoCH/results/ALSPAC_MALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 
+cp ~/EPoCH/results/ALSPAC_MALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/alspac/results/
 cp ~/EPoCH/results/BIB_MALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/bib/results/
 cp ~/EPoCH/results/MCS_MALE_model* /projects/MRC-IEU/research/projects/ieu2/p5/015/working/data/mcs/results/
 
